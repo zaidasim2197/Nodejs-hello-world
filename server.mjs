20f246b9198dda6b2061865645c750b3798d17a8
osamamasood3018 <https://github.com/osamamasood3018/Operation.git>
2022-09-21 16:29:32 +0500

    zaid server

--- server.mjs
+++ server.mjs
@@ -4,7 +4,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log("Hello World! From Zaids Computer")
+  res.send('Hello World! From Zaids Computer')
 })
 
 app.listen(port, () => {
